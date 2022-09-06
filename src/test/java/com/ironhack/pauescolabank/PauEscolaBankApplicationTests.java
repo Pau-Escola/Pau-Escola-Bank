2da2872c32392275ca88fdfc52bfe67dcafdac9b
Pau Escola <pauescola12@gmail.com>
2022-09-06 20:37:37 +0200

    First commit

---
+++ src/test/java/com/ironhack/pauescolabank/PauEscolaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.pauescolabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauEscolaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
